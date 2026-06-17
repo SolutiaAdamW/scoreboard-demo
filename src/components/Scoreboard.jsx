263620469ae6ffa2df5cda42af16b9638f332fea
Adam Wetmore <adam_wetmore@solutiaconsulting.com>
2026-06-17 16:35:52 -0500

    local storage useEffect

--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -4,11 +4,16 @@ import PlayerForm from './PlayerForm';
 import { useState, useEffect } from 'react';
 
 export default function Scoreboard() {
-  const [homePlayers, setHomePlayers] = useState([
-    { name: 'Player 1', number: 10, points: 0 },
-    { name: 'Player 2', number: 20, points: 0 },
-    { name: 'Player 3', number: 30, points: 0 },
-  ]);
+  const loadHomePlayers = () => {
+    const storedValue = localStorage.getItem('homePlayers');
+    const defaultHomePlayers = [
+      { name: 'Player 1', number: 10, points: 0 },
+      { name: 'Player 2', number: 20, points: 0 },
+      { name: 'Player 3', number: 30, points: 0 },
+    ];
+    return storedValue ? JSON.parse(storedValue) : defaultHomePlayers;
+  };
+  const [homePlayers, setHomePlayers] = useState(loadHomePlayers());
   const homeScore = homePlayers.reduce(
     (total, player) => total + player.points,
     0,
@@ -26,11 +31,20 @@ export default function Scoreboard() {
     setHomePlayers((prevPlayers) => [...prevPlayers, player]);
   };
 
-  const [awayPlayers, setAwayPlayers] = useState([
-    { name: 'Player A', number: 11, points: 0 },
-    { name: 'Player B', number: 22, points: 0 },
-    { name: 'Player C', number: 33, points: 0 },
-  ]);
+  useEffect(() => {
+    localStorage.setItem('homePlayers', JSON.stringify(homePlayers));
+  }, [homePlayers]);
+
+  const loadAwayPlayers = () => {
+    const storedValue = localStorage.getItem('awayPlayers');
+    const defaultAwayPlayers = [
+      { name: 'Player A', number: 11, points: 0 },
+      { name: 'Player B', number: 22, points: 0 },
+      { name: 'Player C', number: 33, points: 0 },
+    ];
+    return storedValue ? JSON.parse(storedValue) : defaultAwayPlayers;
+  };
+  const [awayPlayers, setAwayPlayers] = useState(loadAwayPlayers());
   const awayScore = awayPlayers.reduce(
     (total, player) => total + player.points,
     0,
@@ -47,6 +61,10 @@ export default function Scoreboard() {
   const addAwayPlayer = (player) => {
     setAwayPlayers((prevPlayers) => [...prevPlayers, player]);
   };
+
+  useEffect(() => {
+    localStorage.setItem('awayPlayers', JSON.stringify(awayPlayers));
+  }, [awayPlayers]);
 
   return (
     <div className='flex'>
